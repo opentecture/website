595601fe3ae89f3417735c3f1c72c6d60ec2cc4d
Ryan Wold <wold@afomi.com>
2019-01-26 23:59:18 -0800

    add resources page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "blog(/:id)", to: "site#blog_entry"
   get "concept", to: "site#concept"
   get "catalog", to: "site#catalog"
+  get "resources", to: "site#resources"
   get "community", to: "site#community"
   get "origins", to: "site#origins"
   get "stream", to: "site#stream"
